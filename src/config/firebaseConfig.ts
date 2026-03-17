1ce2f6508fccb3b390a4b75ed994c7c8fe554b9a
Jaskarandeep Bhandol <jbhandol@rrc.ca>
2026-03-16 23:35:35 -0600

    feat: configure Firebase Admin SDK for authentication and Firestore

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -1,7 +1,7 @@
 import { initializeApp, cert, ServiceAccount } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
-import serviceAccount from "../firebase-service-account.json";
+import serviceAccount from "../firebase-service-account..json";
 
 initializeApp({
     credential: cert(serviceAccount as ServiceAccount),
